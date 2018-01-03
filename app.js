6a494513eb2f90b134514d2b7fab46208510d19c
himanshu <himanshumehta1114@gmail.com>
2018-01-03 19:08:57 +0530

    updated index file

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const multer = require("multer");
+const body_parser = require("body-parser");
+
+app.use(body_parser.json());
 
 app.set("view engine", "ejs");
 
 app.get('/', (req,res) => {
     res.render('index');
+});
+
+app.post('/size', multer({dest : './uploads/'}).single('photo'), function(req,res){
+    var size = req.file.size;
+    res.send({
+        size : size
+});
+
 });
 
 app.listen(port, () => {
